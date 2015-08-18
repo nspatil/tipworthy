e34adc7429177741a463836b320603d1d1d5e847
Nimish Patil <nimish.patil@gmail.com>
2015-08-18 12:44:33 -0700

    documentation for collect_context

--- collect_context.py
+++ collect_context.py
@@ -6,8 +6,13 @@ from time import sleep
 import pandas
 import sys
 
-def write_context(contexturl, myheaders, attemptnum = 1):
-	if attemptnum > 3:
+#########
+# This is the data collection file. It assumes a csv file with a list of Reddit context
+# URLs exist. It scrapes reddit, attempting to maintain the limit of 30 requests per minute
+# and gathers the top comment or posting for each provided link. It outputs the result as a CSV.
+#########
+def write_context(contexturl, myheaders, attemptnum = 1): #for a given context url, write context will attempt to retrieve the context 3 times. If it fails
+	if attemptnum > 3:                                    #it will return "404 context not found"
 		return '404 context not found'
 	sleep(2.1)
 	try: 
@@ -22,7 +27,7 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		myheaders = {'User-agent': *********} # Insert your own user agent here
 		for i in xrange(1, sys.argv):
-			df = pd.read_csv(sys.argv[i])
+			df = pd.read_csv(sys.argv[i]) #Pass in the CSV files as command line arguments. This program takes an arbitrary number of files
 			df['context'] = ""
 			for index, row in df.iterrows():
 				df.loc[index, 'context'] = str(write_context(row['context_url'], myheaders))
